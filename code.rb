472dcf646c6a21de2ead3220fab6b87e543cdc4a
philiptang42 <philiptang42@gmail.com>
2015-11-01 18:57:28 -0500

    one solution

--- code.rb
+++ code.rb
@@ -1,3 +1,7 @@
 def duplicate_counter(array)
-
+  repeats = array.length - array.uniq.length
 end
+
+puts duplicate_counter([1, 7, 7, 7, 3, 5])
+puts duplicate_counter(([5, 7, 9]))
+puts duplicate_counter([0,-5,-5,33,33,33])
